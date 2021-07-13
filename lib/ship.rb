f602d09812e667f52ccca64639399b81eb11d125
Christina Delpone <cdelpone@gmail.com>
2021-07-13 13:45:57 -0600

    Create Ship class and spec

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,2 +1,24 @@
 class Ship
+  attr_reader :name,
+              :length,
+              :health
+
+  def initialize(name, length)
+    @name = name
+    @length = length
+    @hit_count = 0
+  end
+
+  def health
+    @length - @hit_count
+  end
+
+  def sunk?
+    @hit_count >= 3
+  end
+
+  def hit
+    @hit_count += 1
+  end
+
 end
