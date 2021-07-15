da44553f61e75b0d2696ea754b1f4d158f51a95e
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-15 07:15:00 -0600

    Adjust Board class test to be_a

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,13 +2,13 @@ require './lib/ship.rb'
 require './lib/cell.rb'
 require './lib/board.rb'
 
-Rspec.describe Board do
+RSpec.describe Board do
   before(:each) do
     @board = Board.new
   end
   describe 'board' do
-    xit 'is a Board' do
-      expect(@board).to eq(Board)
+    it 'is a Board' do
+      expect(@board).to be_a(Board)
     end
   end
 end
