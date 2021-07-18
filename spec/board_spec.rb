7378c41ae498f65194f1c88509b5526d2c171010
Christina Delpone <cdelpone@gmail.com>
2021-07-18 15:57:04 -0600

    Write place ship tests

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -55,4 +55,20 @@ RSpec.describe Board do
       expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to eq(true)
     end
   end
+
+  describe 'place ships' do
+    it 'places a ship' do
+      @board.place(@cruiser, ["A1", "A2", "A3"])
+
+      cell_1 = @board.cells["A1"]
+      cell_2 = @board.cells["A2"]
+      cell_3 = @board.cells["A3"]
+
+      expect(cell_1.ship).to eq(@cruiser)
+      expect(cell_2.ship).to eq(@cruiser)
+      expect(cell_3.ship).to eq(@cruiser)
+
+      expect(cell_3.ship == cell_2.ship).to eq(true)
+    end
+  end
 end
