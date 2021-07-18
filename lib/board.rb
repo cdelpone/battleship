a4ae242bc6441d4ec09bb359b53a32856c72807c
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-18 10:51:03 -0600

    Create helper methods for valid_placement and build logic inside method

--- lib/board.rb
+++ lib/board.rb
@@ -34,14 +34,14 @@ class Board
 #figure out how make array of coordinates not loop
 # more logic before accum - need to break up letters and numbers,
 # both letters and numbers need to be consecutive
-  def possible_placements(ship)
-    accum = []
-    included_coordinates.each_cons(ship.length) do |coords_arr|
-      # if consecutive, then shovel
-        accum << coords_arr
-    end
-    accum
-  end
+  # def possible_placements(ship)
+  #   accum = []
+  #   included_coordinates.each_cons(ship.length) do |coords_arr|
+  #     # if consecutive, then shovel
+  #       accum << coords_arr
+  #   end
+  #   accum
+  # end
 
   def possible_letters(ship)
     range = "A".."D"
@@ -63,31 +63,62 @@ class Board
     accum_nums
   end
 
-  def valid_placement?(ship, coordinates)
+  def correct_size?(ship, coordinates)
     ship.length == coordinates.length
-    possible_placements(ship).include?(coordinates)
-require "pry"; binding.pry
-    coords_flat = coordinates.flat_map do |coordinate|
+  end
+
+  def coords_flat(coordinates)
+    coordinates.flat_map do |coordinate|
       coordinate.split('')
     end
-    coords_flat.all?(possible_letters)
-    coords_flat.all?(possible_numbers)
+  end
+
+  def possible_letters?(ship, coordinates)
+    possible_letters(ship).all?(coords_flat(coordinates))
+  end
+
+  def possible_numbers?(ship, coordinates)
+    possible_numbers(ship).all?(coords_flat(coordinates))
+  end
+
+  def valid_placement?(ship, coordinates)
+    if correct_size?(ship, coordinates) == false
+      false
+    elsif possible_letters?(ship, coordinates) == true && possible_numbers?(ship, coordinates) == false
+      true
+    elsif possible_letters?(ship, coordinates) == false && possible_numbers?(ship, coordinates) == true
+      true
+    else
+      false
+    end
   end
 end
 
-  def consecutive_numbers(coordinates)#(or coords)
-    # numbers = split the value of cell and collect numbers
-    numbers = coordinates.split('')
-    numbers.each_cons(numbers.length) do |num1, num2|
-      num1 + 1 == num2 # or num1 == num1
-      #if the letter is the same, then check the numbers are consec
-      #if the number is the same, then check the letters are consec
-    end
-    # if consecutive_letters is true, then consecutive_numbers is false
-    # if consecutive_numbers is true, then consecutive_letters is false
-  end
-
-  def consecutive_letters(coordinates)#(or coords)
-    # confirm letters are consecutive
-    # D can't have A after it
-  end
+# ship.length == coordinates.length
+#
+#
+# def valid_placement?(ship, coordinates)
+# possible_placements(ship).include?(coordinates)
+#
+# coords_flat = coordinates.flat_map do |coordinate|
+#   coordinate.split('')
+# end
+# coords_flat.all?(possible_letters)
+# coords_flat.all?(possible_numbers)
+#end
+  # def consecutive_numbers(coordinates)#(or coords)
+  #   # numbers = split the value of cell and collect numbers
+  #   numbers = coordinates.split('')
+  #   numbers.each_cons(numbers.length) do |num1, num2|
+  #     num1 + 1 == num2 # or num1 == num1
+  #     #if the letter is the same, then check the numbers are consec
+  #     #if the number is the same, then check the letters are consec
+  #   end
+  #   # if consecutive_letters is true, then consecutive_numbers is false
+  #   # if consecutive_numbers is true, then consecutive_letters is false
+  # end
+  #
+  # def consecutive_letters(coordinates)#(or coords)
+  #   # confirm letters are consecutive
+  #   # D can't have A after it
+  # end
