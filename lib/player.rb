44049bac8b6e0d7a1ffbbf343b678371c336948c
Christina Delpone <cdelpone@gmail.com>
2021-07-20 17:12:14 -0600

    Test explanation renders a board

--- lib/player.rb
+++ lib/player.rb
@@ -1,44 +1,57 @@
 require_relative "board"
 require_relative "ship"
+
+class Player
+
+attr_reader :user, :user_sub, :user_cruiser, :user_board
+
+  def initialize
+    @user = user
+    @user_sub = Ship.new("Submarine", 2)
+    @user_cruiser = Ship.new("Cruiser", 3)
+    @user_board = Board.new
+  end
+
   def player_placement
-    #   #need to convert integer to literal
-    p "I have laid out my ships on the grid. /n You now need to lay out your two ships. /n The #{ship1.name} is #{ship1.length} units long and the  #{ship1.name} is #{ship2.length} units long."
+    #need to convert integer to literal
+    p "I have laid out my ships on the grid. /n You now need to lay out your two ships."
+    "The #{@user_cruiser.name} is #{@user_cruiser.length} units long and the #{@user_sub.name} is #{@user_sub.length} units long."
 
-    @board.render
+    p @user_board.render
 
-    def loop(ship)
-      p "Enter the squares for the #{ship.name} (#{ship.length} spaces):"
+    # def loop(ship)
+      p "Enter the squares for the #{@user_cruiser.name} (#{@user_cruiser.length} spaces):"
     end
+#
+#     loop(@user_cruiser)
+#     user_input = gets.chomp.upcase
+#     until @player_board.valid_placement?(@user_cruiser, user_input)
+#       p "Those are invalid coordinates. Please try again:"
+#       loop
+#     end
+#     #if player_ship_placement is valid
+#         #player ship placed on the board
+#         @board.render(true)
+#         p "Enter the squares for the #{ship2.name} (#{ship2.length} spaces):"
+#     #if player_ship_placement is not valid
+#         p "Enter the squares for the #{ship1.name} (#{ship1.length} spaces):"
+#         player_ship_placement = gets.chomp.upcase
+#         #until player_ship_placement is valid
+#         p "Those are invalid coordinates. Please try again:"
+#         player_ship_placement = gets.chomp.upcase
+#
+#         # then player ship placed on the board
+#         @board.render(true)
+#         p "Enter the squares for the #{ship2.name} (#{ship2.length} spaces):"
+#         #if player_ship_placement is not valid
+#             p "Enter the squares for the #{ship1.name} (#{ship1.length} spaces):"
+#             player_ship_placement = gets.chomp.upcase
+#             #until player_ship_placement is valid
+#             p "Those are invalid coordinates. Please try again:"
+#             player_ship_placement = gets.chomp.upcase
+#               #until player_ship_placement is valid
 
-    loop(@user_cruiser)
-    user_input = gets.chomp.upcase
-    until @player_board.valid_placement?(@user_cruiser, user_input)
-      p "Those are invalid coordinates. Please try again:"
-      loop
-    end
-    #if player_ship_placement is valid
-        #player ship placed on the board
-        @board.render(true)
-        p "Enter the squares for the #{ship2.name} (#{ship2.length} spaces):"
-    #if player_ship_placement is not valid
-        p "Enter the squares for the #{ship1.name} (#{ship1.length} spaces):"
-        player_ship_placement = gets.chomp.upcase
-        #until player_ship_placement is valid
-        p "Those are invalid coordinates. Please try again:"
-        player_ship_placement = gets.chomp.upcase
 
-        # then player ship placed on the board
-        @board.render(true)
-        p "Enter the squares for the #{ship2.name} (#{ship2.length} spaces):"
-        #if player_ship_placement is not valid
-            p "Enter the squares for the #{ship1.name} (#{ship1.length} spaces):"
-            player_ship_placement = gets.chomp.upcase
-            #until player_ship_placement is valid
-            p "Those are invalid coordinates. Please try again:"
-            player_ship_placement = gets.chomp.upcase
-              #until player_ship_placement is valid
-
-  #
   # def player_shot
   #   p "Enter the coordinate for your shot:"
   #   if #valid == false
@@ -47,4 +60,6 @@ require_relative "ship"
   #     player_shot = gets.chomp.upcase
   #   end
   # end
+
+
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,14 +3,20 @@ require 'spec_helper'
 RSpec.describe Player do
   before(:each) do
     @user = Player.new
-    @user_board = Board.new
-    @user_sub = Ship.new("Submarine", 2)
-    @user_cruiser = Ship.new("Cruiser", 3)
   end
 
   describe 'Player' do
     it 'is a User' do
-      expect(@user).to be_a(Player)
+      expect(@user).to be_a Player
+    end
+
+    it 'has a board and ships with attributes' do
+      expect(@user.user_cruiser.name).to eq("Cruiser")
+      expect(@user.user_sub.name).to eq("Submarine")
+    end
+
+    it 'can start with an empty board' do
+      expect(@user.user_board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
     end
   end
 end
