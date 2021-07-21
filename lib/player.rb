491a1c6d84d5eab2e8d73cd56e8b02f3e4da2fe9
Christina Delpone <cdelpone@gmail.com>
2021-07-20 19:29:10 -0600

    Validate coordinates

--- lib/player.rb
+++ lib/player.rb
@@ -16,6 +16,12 @@ attr_reader :user, :user_sub, :user_cruiser, :user_board
   #   user_input = gets.chomp.upcase.split(" ")
   # end
 
+    def valid_coordinates?(coordinates)
+      coordinates.any? do |coordinate|
+        valid_coordinate?(coordinate) == false
+      end
+    end
+
   def player_placement
     p "I have laid out my ships on the grid. \nYou now need to lay out your two ships. \nThe #{@user_cruiser.name} is #{@user_cruiser.length} units long and the #{@user_sub.name} is #{@user_sub.length} units long."
 
@@ -29,7 +35,7 @@ attr_reader :user, :user_sub, :user_cruiser, :user_board
     user_input = gets.chomp.upcase.split(" ")
     until @user_board.valid_placement?(@user_cruiser, user_input)
       p "Those are invalid coordinates. Please try again:"
-      loop(@user_cruiser)
+      user_input = gets.chomp.upcase.split(" ")
     end
       @user_board.place(@user_cruiser, user_input)
       p @user_board.render(true)
@@ -38,7 +44,7 @@ attr_reader :user, :user_sub, :user_cruiser, :user_board
     user_input = gets.chomp.upcase.split(" ")
     until @user_board.valid_placement?(@user_sub, user_input)
       p "Those are invalid coordinates. Please try again:"
-      loop(@user_sub)
+    user_input = gets.chomp.upcase.split(" ")
     end
       @user_board.place(@user_sub, user_input)
       p @user_board.render(true)
